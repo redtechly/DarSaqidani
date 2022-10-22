e24458377169246078b712fa103bcfb629e50989
MohamedElk6 <54820708+MohamedElk6@users.noreply.github.com>
2022-10-22 23:42:31 +0200

    Add files via upload

---
+++ frontend/src/screens/OldHomeScreen.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+import Product from '../components/Product';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
+import { listTopSellers } from '../actions/userActions';
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export default function HomeScreen() {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  const userTopSellersList = useSelector((state) => state.userTopSellersList);
+  const {
+    loading: loadingSellers,
+    error: errorSellers,
+    users: sellers,
+  } = userTopSellersList;
+
+  useEffect(() => {
+    dispatch(listProducts({}));
+    dispatch(listTopSellers());
+  }, [dispatch]);
+  return (
+    <div>
+      <Helmet>
+        <title>Diala Store</title>
+      </Helmet>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+          <LoadingBox />
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </div>
+    </div>
+  );
+}
